157c42b464b127c677e92e54f8ac2532dd079eac
RezaIYT <reza.kharamani@studenti.polito.it>
2024-10-16 12:50:26 +0200

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calculate/', views.calculate, name='calculate'),  # Register the calculate URL
+    path('calculate/', views.calculate, name=['calculate']),  # Register the calculate URL
 ]
