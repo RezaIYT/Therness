74166672539bae5c2ed5699b8408e5b9eea4d51e
RezaIYT <reza.kharamani@studenti.polito.it>
2024-10-16 12:46:15 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,5 @@ from api.views import calculate
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('calculate/', calculate.as_view(), name='calculate'),
 ]
